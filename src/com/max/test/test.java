68cdcb2617d49bf0c7791c457c8e3ba4ee9fbb7c
George Young <gmyoungwps@gmail.com>
2019-01-17 10:00:45 -0700

    Initial commit

---
+++ src/com/max/test/test.java
@@ -0,0 +1,9 @@
+package com.max.test;
+
+public class test {
+
+    public static void main(String[] args){
+        System.out.println("Hello World,  This is my first intellij program");
+    }
+
+}
